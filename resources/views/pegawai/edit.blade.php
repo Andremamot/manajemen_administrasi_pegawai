280698abdc31d33af28d20471c20be873886f48c
Andremamot <andreyulidiantoni1@gmail.com>
2025-02-04 17:34:55 +0700

    mengubah edit

--- resources/views/pegawai/edit.blade.php
+++ resources/views/pegawai/edit.blade.php
@@ -44,25 +44,21 @@
                 <div class="col-md-6">
                     <label for="nama">Nama Lengkap</label>
                     <input name="nama" type="text" class="form-control bg-light" id="nama" placeholder="Nama Lengkap" value="{{$guru->nama}}" required oninvalid="this.setCustomValidity('Isian ini tidak boleh kosong !')" oninput="setCustomValidity('')">
-                    <label for="nama">NIP</label>
-                    <input name="nama" type="text" class="form-control bg-light" id="nama" placeholder="Nama Lengkap" value="{{$guru->nama}}" required oninvalid="this.setCustomValidity('Isian ini tidak boleh kosong !')" oninput="setCustomValidity('')">
-                    <label for="jenis_kelamin">Jabatan</label>
-                    <select name="jenis_kelamin" id="jenis_kelamin" class="form-control bg-light" required oninvalid="this.setCustomValidity('Isian ini tidak boleh kosong !')" oninput="setCustomValidity('')">
-                        <option value="Laki-Laki" @if ($guru->jenis_kelamin == 'Laki-Laki') selected @endif>-</option>
-                        <option value="Perempuan" @if ($guru->jenis_kelamin == 'Perempuan') selected @endif>-</option>
+                    <label for="nip">NIP</label>
+                    <input name="nip" type="text" class="form-control bg-light" id="nama" placeholder="nomor NIP" value="{{$guru->nama}}" required oninvalid="this.setCustomValidity('Isian ini tidak boleh kosong !')" oninput="setCustomValidity('')">
+                    <label for="jabatan">Jabatan</label>
+                    <select name="jabatan" id="jenis_kelamin" class="form-control bg-light" required oninvalid="this.setCustomValidity('Isian ini tidak boleh kosong !')" oninput="setCustomValidity('')">
+                        <option value="1" @if ($guru->jenis_kelamin == '1') selected @endif>-</option>
+                        <option value="2" @if ($guru->jenis_kelamin == '2') selected @endif>-</option>
                     </select>
-                    <label for="tempat_lahir">Jabatan</label>
-                    <input name="tempat_lahir" type="text" class="form-control bg-light" id="tempat_lahir" placeholder="Tempat Lahir" value="{{$guru->tempat_lahir}}" required oninvalid="this.setCustomValidity('Isian ini tidak boleh kosong !')" oninput="setCustomValidity('')">
-                    <label for="tanggal_lahir">departemen</label>
-                    <input name="tanggal_lahir" type="date" class="form-control bg-light" id="tanggal_lahir" value="{{$guru->tanggal_lahir}}" required oninvalid="this.setCustomValidity('Isian ini tidak boleh kosong !')" oninput="setCustomValidity('')">
+                    <label for="departemen">departemen</label>
+                    <input name="departemen" type="date" class="form-control bg-light" id="departemen" placeholder="departemen" value="{{$guru->tanggal_lahir}}" required oninvalid="this.setCustomValidity('Isian ini tidak boleh kosong !')" oninput="setCustomValidity('')">
                 </div>
                 <div class="col-md-6">
-                    <label for="alamat">Alamat Lengkap</label>
-                    <textarea name="alamat" class="form-control bg-light" id="alamat" rows="2" placeholder="Alamat" value="{{$guru->alamat}}" required oninvalid="this.setCustomValidity('Isian ini tidak boleh kosong !')" oninput="setCustomValidity('')">{{$guru->alamat}}</textarea>
-                    <label for="no_hp">Nomor HP</label>
-                    <input name="no_hp" type="number" class="form-control bg-light" id="no_hp" placeholder="Nomor HP" value="{{$guru->no_hp}}" required oninvalid="this.setCustomValidity('Isian ini tidak boleh kosong !')" oninput="setCustomValidity('')">
-                    <label for="email">Email</label>
-                    <input name="email" type="email" class="form-control bg-light" id="email" placeholder="Email" value="{{$guru->email}}" required email oninvalid="this.setCustomValidity('Pastikan anda sudah mengisikan email dengan benar !')" oninput="setCustomValidity('')">
+                    <label for="gaji">gaji</label>
+                    <textarea name="Gaji" class="form-control bg-light" id="alamat" rows="2" placeholder="gaji" value="{{$guru->alamat}}" required oninvalid="this.setCustomValidity('Isian ini tidak boleh kosong !')" oninput="setCustomValidity('')">{{$guru->alamat}}</textarea>
+                    <label for="join_date">tanggal bergabung</label>
+                    <input name="join_date" type="number" class="form-control bg-light" id="no_hp" placeholder="tanggal bergabung" value="{{$guru->no_hp}}" required oninvalid="this.setCustomValidity('Isian ini tidak boleh kosong !')" oninput="setCustomValidity('')">
                 </div>
             </div>
             <hr>
